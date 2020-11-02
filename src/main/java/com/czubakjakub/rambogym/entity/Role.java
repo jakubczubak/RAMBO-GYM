60f819a0efe70c024cedf154e0feb434eb35d9ce
Jakub Czubak <czubakjakub94@gmail.com>
2020-11-02 22:43:42 +0100

    add new entity - Role

---
+++ src/main/java/com/czubakjakub/rambogym/entity/Role.java
@@ -0,0 +1,17 @@
+package com.czubakjakub.rambogym.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private long id;
+    @Column(name = "role")
+    private String name;
+}
